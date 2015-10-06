389c9bc546dc1b69520ff2c1fa4b1800d2b8b478
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2015-10-06 14:28:58 +0200

    added global variables

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,18 @@
 
 public class Driver {
+	
+	// Please note that these numbers are probably bullshit, as I have
+	// no idea what half of these things mean
+	public static final int MAX_ITERATIONS = 10;
+	public static final int NUMBER_OF_ANTS = 1000;
+	public static final double PHEROMONE = 832;
+	public static final double EVAPORATION = 0.5;
+	public static final double CONVERGENCE_CRITERIA = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Yoooooooooooo");
-
+		
 		//Bla
 
 	}
